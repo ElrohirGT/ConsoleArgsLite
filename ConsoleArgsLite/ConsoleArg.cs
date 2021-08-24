3fa1e29749e540832b4c3e1d2a5e9a814546d050
ElrohirGT <elrohirgt@gmail.com>
2021-08-24 12:35:58 -0600

    Add project files.

---
+++ ConsoleArgsLite/ConsoleArg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleArgsLite
+{
+    internal class ConsoleArg<T>
+    {
+        public ConsoleArg(string name, Func<string, T> parser, bool required, string consoleName = null)
+        {
+            Name = name;
+            Parser = parser;
+            Required = required;
+            ConsoleName = consoleName;
+        }
+
+        public string Name { get; }
+        public string ConsoleName { get; }
+        public string ConsoleValue { get; }
+        public T Value { get; private set; }
+        public Func<string, T> Parser { get; }
+        public bool Required { get; }
+
+        public void Parse(string input) => Value = Parser(input);
+    }
+}
---
+++ ConsoleArgsLite/ConsoleArgsManagerLite.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleArgsLite
+{
+    public class ConsoleArgsManagerLite
+    {
+        private List<KeyValuePair<Type, ConsoleArg<object>>> _arguments = new List<KeyValuePair<Type, ConsoleArg<object>>>();
+
+        /// <summary>
+        /// Adds a string argument.
+        /// </summary>
+        /// <param name="name">The name of the argument, this value must be unique.</param>
+        /// <param name="required">Whether or not the argument is required.</param>
+        /// <param name="consoleName">The name of the argument in the console (do not include --).</param>
+        public void AddArgument(string name, bool required = true, string consoleName = null) => AddArgument(name, required, (s) => s, consoleName);
+
+        /// <summary>
+        /// Adds an argument of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the argument.</typeparam>
+        /// <param name="name">The name of the argument, this value must be unique.</param>
+        /// <param name="required">Whether or not the value is required.</param>
+        /// <param name="parser">The converter from the string value from the cmd to a type T.</param>
+        /// <param name="consoleName">The name of the argument in the console (do not include --).</param>
+        public void AddArgument<T>(string name, bool required, Func<string, T> parser, string consoleName = null)
+        {
+            Func<string, object> converter = (s) => parser(s);
+            _arguments.Add(KeyValuePair.Create(typeof(T), new ConsoleArg<object>(name, converter, required, consoleName)));
+        }
+
+        public T GetValueFromArgument<T>(string Name) => (T)_arguments.Find((v) => v.Value?.Name == Name).Value?.Value;
+
+        public void ParseArguments(string[] args)
+        {
+            int currentArgsIndex = 0;
+            foreach (var keyValuePair in _arguments)
+            {
+                if (currentArgsIndex < args.Length)
+                    ParseArgument(keyValuePair.Value, args, ref currentArgsIndex);
+                else if (keyValuePair.Value.Required)
+                    throw new ArgumentNullException(keyValuePair.Value.Name);
+            }
+        }
+
+        private static void ParseArgument(ConsoleArg<object> argument, string[] args, ref int currentIndex)
+        {
+            if (string.IsNullOrEmpty(argument.ConsoleName))
+            {
+                if (TryParsingArgument(args[currentIndex], argument))
+                    ++currentIndex;
+            }
+            else if ($"--{argument.ConsoleName}" == args[currentIndex])
+            {
+                int valueIndex = currentIndex + 1;
+                if (valueIndex >= args.Length && argument.Required)
+                    throw new ArgumentNullException(argument.Name);
+
+                if (TryParsingArgument(args[valueIndex], argument))
+                    currentIndex = valueIndex + 1;
+            }
+        }
+
+        /// <summary>
+        /// Tries to parse an argument from the supplied consoleArgument.
+        /// </summary>
+        /// <param name="consoleArgument">The string supplied in the commandline.</param>
+        /// <param name="argument">The argument object.</param>
+        /// <returns>True if it succesfully parsed the object, false otherwise.</returns>
+        private static bool TryParsingArgument(string consoleArgument, ConsoleArg<object> argument)
+        {
+            try
+            {
+                bool consoleArgIsAFlag = consoleArgument.StartsWith("--");
+                if (consoleArgIsAFlag)
+                {
+                    if (argument.Required)
+                        throw new ArgumentNullException(argument.Name);
+                    else
+                        return false;
+                }
+                argument.Parse(consoleArgument);
+                return true;
+            }
+            catch (ArgumentNullException) { throw; }
+            catch (Exception e)
+            {
+                throw new ArgumentException($"Couldn't parse the {argument.Name} argument", argument.Name, e);
+            }
+        }
+    }
+}
