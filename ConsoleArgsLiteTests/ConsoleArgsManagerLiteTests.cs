d237cec7bab53878d3f960337165bd11a11dc2ae
ElrohirGT <elrohirgt@gmail.com>
2021-08-24 13:57:18 -0600

    Added some tests

--- ConsoleArgsLiteTests/ConsoleArgsManagerLiteTests.cs
+++ ConsoleArgsLiteTests/ConsoleArgsManagerLiteTests.cs
@@ -6,6 +6,7 @@ namespace ConsoleArgsLiteTests
 {
     public class ConsoleArgsManagerLiteTests
     {
+        #region Simple Tests
         [Fact]
         public void ParsesSimpleArgument()
         {
@@ -29,5 +30,31 @@ namespace ConsoleArgsLiteTests
 
             Assert.Equal("output.mp4", manager.GetValueFromArgument<string>("output"));
         }
+        [Fact]
+        public void ParsesSimpleMissingOptionalArgument()
+        {
+            string[] args = new string[0];
+            ConsoleArgsManagerLite manager = new ConsoleArgsManagerLite();
+            manager.AddArgument("output", false);
+
+            manager.ParseArguments(args);
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void ReturnsNullWhenGettingAnUnparsedArgument()
+        {
+            string[] args = new string[0];
+            ConsoleArgsManagerLite manager = new ConsoleArgsManagerLite();
+            manager.AddArgument("output", false);
+
+            manager.ParseArguments(args);
+
+            string argument = manager.GetValueFromArgument<string>("output");
+
+            Assert.Null(argument);
+        }
+        #endregion
+        
     }
 }
